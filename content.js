8be94748a98d93a9cf9e660043801fd88faedf91
SebastianMB-IT <sebastian.besel@nethesis.it>
2019-06-06 09:59:21 +0200

    Apply colors though chat is closed

--- content.js
+++ content.js
@@ -11,17 +11,17 @@ function setColor () {
                 var gradientText = "-webkit-linear-gradient(" + first + ", " + second + ", " + third + ")";
                 var colorFixedBorderBottom = "2px solid " + third + ";";
                 var colorIcon = third + " !important;";
-                var selectorBar = "body > div.view-scaffold.ng-scope > div.ui.borderless.top.fixed.inverted.hidden.menu.blue.fix-height.ng-scope";
-                var selectorSideBarHeader = "body > div.view-scaffold.ng-scope > div.ui.pushable.right.raised.sidebar.segments.short-action.overlay.visible.right-sidebar-container.right.toggle-right-sidebar-hide > div.ui.segment.right-sidebar-up > div > div.ui.top.right.pointing.content.dropdown.adjust-responsive-padding.speed-dropdown > h4";
-                var selectorSideBarSec = "body > div.view-scaffold.ng-scope > div.ui.pushable.right.raised.sidebar.segments.short-action.overlay.visible.right-sidebar-container.right.toggle-right-sidebar-hide > div.ui.segment.right-sidebar-down > h4";
+                var selectorBar = "body > div.view-scaffold > div.ui.borderless.top.fixed.inverted.hidden.menu.blue.fix-height";
+                var selectorSideBarHeader = "body > div.view-scaffold > div.ui.pushable.right.raised.sidebar.segments.short-action.overlay.visible.right-sidebar-container.right.toggle-right-sidebar-hide > div.ui.segment.right-sidebar-up > div > div.ui.top.right.pointing.content.dropdown.adjust-responsive-padding.speed-dropdown > h4";
+                var selectorSideBarSec = "body > div.view-scaffold > div.ui.pushable.right.raised.sidebar.segments.short-action.overlay.visible.right-sidebar-container.right.toggle-right-sidebar-hide > div.ui.segment.right-sidebar-down > h4";
                 var firstSideBarIcon = "#speedDialSettingsIcon";
-                var secondSideBarIcon = "body > div.view-scaffold.ng-scope > div.ui.pushable.right.raised.sidebar.segments.short-action.overlay.visible.right-sidebar-container.right.toggle-right-sidebar-hide > div.ui.segment.right-sidebar-down > i";
+                var secondSideBarIcon = "body > div.view-scaffold > div.ui.pushable.right.raised.sidebar.segments.short-action.overlay.visible.right-sidebar-container.right.toggle-right-sidebar-hide > div.ui.segment.right-sidebar-down > i";
                 var chatSideBarSel = "#headerIdChat";
                 var chatSideBarIcon = "#headerIdChat > i";
 
                 // check elements exists
                 var checkExist = setInterval(function() {
-                    if ($(selectorBar).length && $(selectorSideBarHeader).length && $(selectorSideBarSec).length && $(chatSideBarSel).length) {
+                    if ($(selectorBar).length && $(selectorSideBarHeader).length && $(selectorSideBarSec).length) {
                         // set div style
                         $(selectorBar).attr("style", "background: " + gradientDiv);
 
